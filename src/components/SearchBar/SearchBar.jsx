573b779714fc7dbcccad4a8a8a71d350f0494122
frankynztein <arellanfranciree@gmail.com>
2025-02-13 23:39:21 +0100

    Search bar structure

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,7 +3,28 @@ import './SearchBar.css';
 const SearchBar = () => {
   return (
     <>
-      <h1>Search Bar here</h1>
+      <div className="search-bar">
+        <form action="/search-cars" method="GET">
+          <div className='search-bar-input'>
+            <label htmlFor="car-category">Categoría del coche</label>
+            <input type="text" id="carCategory" name="car-category" placeholder="Deportivo"/>
+          </div>
+
+          <div className='search-bar-input'>
+            <label htmlFor="pickup-date">Recogida</label>
+            <input type="date" id="pickup-date" name="pickup-date"/>
+            <input type="time" id="pickup-time" name="pickup-time"/>
+          </div>
+
+          <div className='search-bar-input'>
+            <label htmlFor="dropoff-date">Devolución</label>
+            <input type="date" id="dropoff-date" name="dropoff-date"/>
+            <input type="time" id="dropoff-time" name="dropoff-time"/>
+          </div>
+
+          <button type="submit">Buscar</button>
+        </form>
+      </div>
     </>
   )
 }
