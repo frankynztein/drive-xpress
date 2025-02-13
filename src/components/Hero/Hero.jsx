3b0675562e739007e60e2d31cfc6944951364f3c
frankynztein <arellanfranciree@gmail.com>
2025-02-13 23:40:10 +0100

    Added Hero and Cards folder structure

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <>
+      <div className='hero-wrapper'>
+        <div className='hero'>
+          <h1>aqui va un hero</h1>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export { Hero };
